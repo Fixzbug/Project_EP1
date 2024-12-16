5f56ac2e2dc8fff852b77eb4be30db5541de9450
Makerz <makerz03795@hotmail.com>
2024-12-16 13:49:47 +0700

    update: Dockerfile :fire

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const app = express();
-const port = 8300;
+var express = require('express');
+var cors = require('cors');
+const mysql = require('mysql2');
+const port = 5000;
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+var app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World! 1');
+});
+
+app.get('/attractions', function(req, res, next) {
+  pool.query("SELECT * FROM attractions", function(err, rows, fields) {
+    res.json(rows);
+  });
 });
 
 app.listen(port, () => {
