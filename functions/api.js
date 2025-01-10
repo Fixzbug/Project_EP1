de21f05ec786b2fc0be80990fe2005debbb46971
Makerz <makerz03795@hotmail.com>
2025-01-10 16:55:30 +0700

    changes

---
+++ functions/api.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ServerlessHttp = require("serverless-http");
+
+const app = express();
+
+app.get('/.netlify/functions/api', (req, res) => {
+    return res.json({
+        messages: "hello world!"
+    });
+});
+
+const handler = ServerlessHttp(app);
+
+module.exports.handler = async (event, context) => {
+    const result = await handler(event, context);
+    return result;
+};
