26c9f9ec28d37efa22a473d69a802b9fadc1a312
Makerz <makerz03795@hotmail.com>
2025-01-10 14:58:30 +0700

    update: fixbug :fire:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2/promise'); // Use promise-based MySQL
+// const mysql = require('mysql2/promise'); // Use promise-based MySQL
 require('dotenv').config();
 
 const app = express();
@@ -12,16 +12,16 @@ const port = process.env.PORT || 5000; // Fallback to 5000 if PORT is not define
 //   process.exit(1);
 // }
 
-// MySQL Connection Pool
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// // MySQL Connection Pool
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 app.use(cors()); // Enable CORS
 app.use(express.json()); // Support for JSON payloads
@@ -31,24 +31,24 @@ app.get('/', (req, res) => {
   res.send(`Hello World! v ${process.env.APP_VERSION || '1.0.0'}`);
 });
 
-// Get Attractions Route
-app.get('/attractions', async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT * FROM attractions");
-    res.json(rows);
-  } catch (err) {
-    console.error("Database error: ", err.message);
-    res.status(500).json({ error: "Database query failed" });
-  }
-});
+// // Get Attractions Route
+// app.get('/attractions', async (req, res) => {
+//   try {
+//     const [rows] = await pool.query("SELECT * FROM attractions");
+//     res.json(rows);
+//   } catch (err) {
+//     console.error("Database error: ", err.message);
+//     res.status(500).json({ error: "Database query failed" });
+//   }
+// });
 
-// Graceful Shutdown
-process.on('SIGINT', () => {
-  pool.end(() => {
-    console.log("Database connection pool closed.");
-    process.exit(0);
-  });
-});
+// // Graceful Shutdown
+// process.on('SIGINT', () => {
+//   pool.end(() => {
+//     console.log("Database connection pool closed.");
+//     process.exit(0);
+//   });
+// });
 
 // Start the Server
 app.listen(port, () => {
