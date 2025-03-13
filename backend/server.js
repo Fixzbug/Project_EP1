3b9ed1cb596cdf2047f39c5af39c1bcbd6b78cd5
unknown <makerz03795@hotmail.com>
2025-03-13 10:31:20 +0700

    update: fixed :fire:

--- backend/server.js
+++ backend/server.js
@@ -46,8 +46,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.error("MongoDB Connection Error:", err);
   });
 
-
-
 // Root Route
 app.get('/', (req, res) => {
   res.send({
