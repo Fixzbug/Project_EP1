9f9fa3c5254f82a87a77ba6b31ab1a661aebf90a
unknown <makerz03795@hotmail.com>
2025-03-13 11:26:21 +0700

    update: fixed :fire:

--- backend/server.js
+++ backend/server.js
@@ -36,17 +36,17 @@ mqttClient.on("error", (err) => {
 });
 
 // เชื่อมต่อ MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  // useNewUrlParser: true, useUnifiedTopology: true
-})
-  .then(() => {
-    isMongoConnected = true;
-    console.log("Connected to MongoDB");
-  })
-  .catch(err => {
-    isMongoConnected = false;
-    console.error("MongoDB Connection Error:", err);
-  });
+// mongoose.connect(process.env.MONGO_URI, {
+//   // useNewUrlParser: true, useUnifiedTopology: true
+// })
+//   .then(() => {
+//     isMongoConnected = true;
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch(err => {
+//     isMongoConnected = false;
+//     console.error("MongoDB Connection Error:", err);
+//   });
 
 // Root Route
 app.get('/', (req, res) => {
