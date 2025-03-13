90924c39500a5832d0bfb58bb375df68b9eeccfe
unknown <makerz03795@hotmail.com>
2025-03-13 11:03:29 +0700

    update: fixed :fire:

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,9 @@ mqttClient.on("error", (err) => {
 });
 
 // เชื่อมต่อ MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {
+  // useNewUrlParser: true, useUnifiedTopology: true
+})
   .then(() => {
     isMongoConnected = true;
     console.log("Connected to MongoDB");
