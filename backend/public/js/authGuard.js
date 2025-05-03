cdf048083632751171e594059d9d5a1c832acb45
unknown <makerz03795@hotmail.com>
2025-05-03 17:28:30 +0700

    update: fixed :fire:

---
+++ backend/public/js/authGuard.js
@@ -0,0 +1,11 @@
+// public/js/authGuard.js
+
+const token = localStorage.getItem('token');
+if (!token) {
+  window.location.href = 'login.html';
+}
+
+function logout() {
+  localStorage.removeItem('token');
+  window.location.href = 'login.html';
+}
